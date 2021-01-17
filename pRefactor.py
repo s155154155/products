69e1d9c5dd556c6d03decb05adf1224a0d5b9b69
NomikLee <nomiklee@liyuanhaode-MBP.lan>
2021-01-17 15:36:47 +0800

    add def

---
+++ pRefactor.py
@@ -0,0 +1,47 @@
+import os
+
+#讀取檔案
+def read_file(filename):
+	products = []
+	if os.path.isfile(filename):
+		with open(filename, 'r', encoding = 'utf-8') as f:
+			for line in f:
+				if '商品,價格' in line:
+					continue
+				name, price = line,strip().split(',')
+				products.append([name, price])
+		print(products)
+
+	else:
+		print('找不到檔案....')
+
+	return products
+
+#讓使用者輸入
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱：')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格：')
+		products.append([name, price])
+	print(products)
+	return products
+
+#印出所有購買紀錄
+def print_products(products):
+	for p in products:
+		print(p[0], '的價格是', p[1])
+
+#寫入檔案
+def write_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
+
+
+products = read_file('products.csv')
+products = user_input(products)
+print_products(products)
+write_file('products.csv', products)
